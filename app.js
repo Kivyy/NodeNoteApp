8ec0f4531c96b60915ed9828d5ff63e99723c2d2
Kiv Lo <kivlokw@gmail.com>
2017-07-08 15:25:46 -0400

    add ternary operator for remove function

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ console.log(argv);
 
 if (command === 'add') {
     let note = notes.addNote(argv.title, argv.body);
-    
+
     if (note){
       console.log("Successfully added note", note )
     } else {
@@ -23,7 +23,9 @@ if (command === 'add') {
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'remove') {
-  notes.removeNote(argv.title);
+  let noteRemoved = notes.removeNote(argv.title);
+  let message = noteRemoved ? 'Note was removed' : 'Note not found'
+  console.log(message);
 } else if (command === 'read') {
   notes.readNote(argv.title);
 } else {
